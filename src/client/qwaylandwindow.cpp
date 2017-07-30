258d1f4426adc8fd74b47c33564122021c9e838f
Robin Burchell <robin.burchell@crimson.no>
2017-07-30 21:50:41 +0200

    Client: Implement wl_pointer version 5
    Version 5 adds frame events, which groups multiple pointer events together,
    enabling diagonal scrolling, leave and enter without an intermediate state, and
    together with the new axis_source, axis_discrete and axis_stop also adds what
    needed to differentiate between wheel events and touchpad scrolling.
    
    This patch adds scroll phases and pixel deltas to QWaylandInputDevice, and
    makes sure handleWheelEvent is called accordingly.
    
    [ChangeLog][QPA plugin] Pixel delta is now set for mouse scrolling events if
    originating from an appropriate device such as a touch pad (requires compositor
    support for wl_seat version 5 or later).
    
    Fixes: QTBUG-69876
    Change-Id: I094a1ef0365893bee135cae7e6df89fafdafa2f2

--- src/client/qwaylandwindow.cpp
+++ src/client/qwaylandwindow.cpp
@@ -817,6 +817,18 @@ QWaylandWindow *QWaylandWindow::transientParent() const
 
 void QWaylandWindow::handleMouse(QWaylandInputDevice *inputDevice, const QWaylandPointerEvent &e)
 {
+    if (e.type == QWaylandPointerEvent::Leave) {
+        if (mWindowDecoration) {
+            if (mMouseEventsInContentArea)
+                QWindowSystemInterface::handleLeaveEvent(window());
+        } else {
+            QWindowSystemInterface::handleLeaveEvent(window());
+        }
+#if QT_CONFIG(cursor)
+        restoreMouseCursor(inputDevice);
+#endif
+        return;
+    }
 
     if (mWindowDecoration) {
         handleMouseEventWithDecoration(inputDevice, e);
@@ -825,11 +837,15 @@ void QWaylandWindow::handleMouse(QWaylandInputDevice *inputDevice, const QWaylan
             case QWaylandPointerEvent::Enter:
                 QWindowSystemInterface::handleEnterEvent(window(), e.local, e.global);
                 break;
+            case QWaylandPointerEvent::Press:
+            case QWaylandPointerEvent::Release:
             case QWaylandPointerEvent::Motion:
                 QWindowSystemInterface::handleMouseEvent(window(), e.timestamp, e.local, e.global, e.buttons, e.modifiers);
                 break;
             case QWaylandPointerEvent::Wheel:
-                QWindowSystemInterface::handleWheelEvent(window(), e.timestamp, e.local, e.global, e.pixelDelta, e.angleDelta, e.modifiers);
+                QWindowSystemInterface::handleWheelEvent(window(), e.timestamp, e.local, e.global,
+                                                         e.pixelDelta, e.angleDelta, e.modifiers,
+                                                         e.phase, e.source, false);
                 break;
         }
     }
@@ -842,20 +858,6 @@ void QWaylandWindow::handleMouse(QWaylandInputDevice *inputDevice, const QWaylan
         if (contentGeometry.contains(e.local.toPoint()))
             restoreMouseCursor(inputDevice);
     }
-#endif
-}
-
-void QWaylandWindow::handleMouseLeave(QWaylandInputDevice *inputDevice)
-{
-    if (mWindowDecoration) {
-        if (mMouseEventsInContentArea) {
-            QWindowSystemInterface::handleLeaveEvent(window());
-        }
-    } else {
-        QWindowSystemInterface::handleLeaveEvent(window());
-    }
-#if QT_CONFIG(cursor)
-    restoreMouseCursor(inputDevice);
 #endif
 }
 
@@ -900,12 +902,18 @@ void QWaylandWindow::handleMouseEventWithDecoration(QWaylandInputDevice *inputDe
             case QWaylandPointerEvent::Enter:
                 QWindowSystemInterface::handleEnterEvent(window(), localTranslated, globalTranslated);
                 break;
+            case QWaylandPointerEvent::Press:
+            case QWaylandPointerEvent::Release:
             case QWaylandPointerEvent::Motion:
                 QWindowSystemInterface::handleMouseEvent(window(), e.timestamp, localTranslated, globalTranslated, e.buttons, e.modifiers);
                 break;
-            case QWaylandPointerEvent::Wheel:
-                QWindowSystemInterface::handleWheelEvent(window(), e.timestamp, localTranslated, globalTranslated, e.pixelDelta, e.angleDelta, e.modifiers);
+            case QWaylandPointerEvent::Wheel: {
+                QWindowSystemInterface::handleWheelEvent(window(), e.timestamp,
+                                                         localTranslated, globalTranslated,
+                                                         e.pixelDelta, e.angleDelta, e.modifiers,
+                                                         e.phase, e.source, false);
                 break;
+            }
         }
 
         mMouseEventsInContentArea = true;
