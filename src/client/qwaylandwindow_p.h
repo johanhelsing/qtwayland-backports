26fd2f267003cd7fc2ab6188d2874b96d8803f4e
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2018-04-27 17:07:26 +0200

    StartSystemMove doesn't exist in 5.11

--- src/client/qwaylandwindow_p.h
+++ src/client/qwaylandwindow_p.h
@@ -201,7 +201,7 @@ public:
     void propagateSizeHints() override { }
     void addAttachOffset(const QPoint point);
 
-    bool startSystemMove(const QPoint &pos) override;
+    bool startSystemMove(const QPoint &pos);
 
     void requestUpdate() override;
 
